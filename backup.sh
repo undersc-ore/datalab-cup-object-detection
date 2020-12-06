3c927550d0cebe356f192c3272774ddf5aa896be
Akitaki <robinhuang123@gmail.com>
2020-12-06 04:53:40 +0000

    Add report part to the notebook

--- backup.sh
+++ backup.sh
@@ -5,7 +5,3 @@ BACKUPDIR=/mnt/usb/compt2-snapshots/${TIME}
 
 mkdir "${BACKUPDIR}"
 rsync -av . "${BACKUPDIR}"
-cd "${BACKUPDIR}/.."
-zip -r "${TIME}.zip" "${TIME}/ckpts/YOLO_VGG/"
-rclone copy -P "${TIME}.zip" 'n:/yolo/'
-cd -
